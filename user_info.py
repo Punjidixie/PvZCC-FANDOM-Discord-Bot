0e3b305574daa602b92bde7afaab8f4d80575c8e
Siraphop Rungtragoolchai <punjidixie@gmail.com>
2023-09-19 23:00:52 +0700

    userinfo format

--- user_info.py
+++ user_info.py
@@ -4,7 +4,7 @@ import groups_parser as gp
 from datetime import datetime
 
 def call_and_get_user_json(username):
-    url_segment = f"api.php?action=query&list=users&usprop=groups|editcount|registration&ususers={username}&format=json"
+    url_segment = f"api.php?action=query&list=users&usprop=groups|editcount|blockinfo&ususers={username}&format=json"
     
     return caller.get_json(url_segment)
 
@@ -13,6 +13,32 @@ def call_and_get_firstedit_json(username):
     
     return caller.get_json(url_segment)
 
+def call_and_get_lastedit_json(username):
+    url_segment = f"api.php?action=query&format=json&list=usercontribs&ucuser={username}&ucdir=older&uclimit=1"
+    
+    return caller.get_json(url_segment)
+
+def time_diff_to_string(diff):
+    
+    num = 0
+    unit = "minute"
+    suffix = 's'
+    
+    if diff.seconds // 60 == 0: return "Just now"
+    elif diff.seconds // 3600 == 0:
+        num = diff.seconds // 60
+        unit = "minute"
+    elif diff.days == 0:
+        num = diff.seconds // 3600
+        unit = "hour"
+    else:
+        num = diff.days
+        unit = "day"
+    
+    if num == 1: suffix = ''
+    
+    return f"{num} {unit}{suffix} ago"
+        
 def generate_response(username):
     
     
@@ -20,15 +46,19 @@ def generate_response(username):
     j = call_and_get_user_json(username)
     user_info = j["query"]["users"][0]
     
-    desc = "This user is on PvZCC!"
+    desc = ":white_check_mark: This user is on PvZCC!"
     
-    # Check of the user has edited on the wiki.
+    # Check of the user has edited on the wiki + is blocked or not
     is_pvzccer = False
+    is_blocked = "blockid" in user_info
+    
     if "missing" in user_info:
-        desc = "This user doesn't exist. Make sure you entered the correct username (case sensitive)."
+        desc = ":grey_question: This user doesn't exist. Make sure you entered the correct username (case sensitive)."
     elif user_info["editcount"] == 0:
-        desc = "This user has never been in PvZCC."
-    else: is_pvzccer = True
+        desc = ":warning: This user has never been in PvZCC."
+    else:
+        is_pvzccer = True
+        if is_blocked: desc = ":no_entry: This user is blocked in PvZCC."
              
     # Base of the embed response
     embed = discord.Embed(
@@ -38,34 +68,44 @@ def generate_response(username):
     
     # Constructing the response body
     if is_pvzccer:
+    
         jf = call_and_get_firstedit_json(username)
         contrib = jf["query"]["usercontribs"][0]
         
-        parsed_join_date = datetime.strptime(contrib["timestamp"], "%Y-%m-%dT%H:%M:%SZ")
-        formatted_join_date = parsed_join_date.strftime("%B %d, %Y")
-        #join_time_difference = datetime.now() - parsed_join_date
+        jl = call_and_get_lastedit_json(username)
+        final_contrib = jl["query"]["usercontribs"][0]
         
-        parsed_reg_date = datetime.strptime(user_info["registration"], "%Y-%m-%dT%H:%M:%SZ")
-        formatted_reg_date = parsed_reg_date.strftime("%B %d, %Y")
-        #reg_time_difference = datetime.now() - parsed_reg_date
+        # Blocked info
+        if is_blocked:
+            formatted_blocked_until = ":skull: *The end of time* :skull:"
+            if user_info["blockexpiry"] != "infinite":
+                parsed_blocked_until = datetime.strptime(user_info["blockexpiry"], "%Y-%m-%dT%H:%M:%SZ")
+                formatted_blocked_until = parsed_blocked_until.strftime("%B %d, %Y")
+            embed.add_field(name="Block expiry",value=formatted_blocked_until, inline = False)
+            
+
+        # Edit count
+        edit_count = user_info["editcount"]
+        embed.add_field(name="Edit count",value=edit_count, inline = False)
         
-    
-        
+        # User group
         groups = user_info["groups"]
         group_name = gp.group_name(gp.highest_group(groups))
-        edit_count = user_info["editcount"]
-        
-        embed.add_field(name="Edit count",value=edit_count, inline = False)
         embed.add_field(name="Group",value=group_name, inline = False)
         
-        embed.add_field(name="First PvZCC edit",value=formatted_join_date)
-        #embed.add_field(name="Days ago",value=join_time_difference.days)
+        # Join date
+        parsed_join_date = datetime.strptime(contrib["timestamp"], "%Y-%m-%dT%H:%M:%SZ")
+        formatted_join_date = parsed_join_date.strftime("%B %d, %Y")
+        embed.add_field(name="First edit",value=formatted_join_date)
         
-        embed.add_field(name="Account creation",value=formatted_reg_date)
-        #embed.add_field(name="Days ago",value=reg_time_difference.days)
+        # Most recent edit
+        parsed_final_date = datetime.strptime(final_contrib["timestamp"], "%Y-%m-%dT%H:%M:%SZ")
+        final_time_difference = datetime.now() - parsed_final_date
+        embed.add_field(name="Most recent edit",value=time_diff_to_string(final_time_difference))
         
-        #embed.add_field(name="First edit time",value=formatted_join_time)
-    
+        #parsed_reg_date = datetime.strptime(user_info["registration"], "%Y-%m-%dT%H:%M:%SZ")
+        #formatted_reg_date = parsed_reg_date.strftime("%B %d, %Y")
+
     return embed
                                       
     
